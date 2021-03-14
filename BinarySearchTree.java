b87d747b625b6694afa2c0548aca2fc456611e0f
InduLekha98 <rindulekha09@gmail.com>
2021-03-14 22:43:36 +0530

    Added:Ability to create binary tree

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -138,6 +138,17 @@ public class BinarySearchTree {
             bt.insert(56);
             bt.insert(30);
             bt.insert(70);
+            bt.insert(22);
+            bt.insert(40);
+            bt.insert(60);
+            bt.insert(95);
+            bt.insert(11);
+            bt.insert(3);
+            bt.insert(16);
+            bt.insert(65);
+            bt.insert(63);
+            bt.insert(67);
+
 
             System.out.println("Binary search tree after insertion:");
             //Displays the binary tree
